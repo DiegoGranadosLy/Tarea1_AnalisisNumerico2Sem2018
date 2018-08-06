c38e083974a07d7e12a8d15e61f6fc3f4caeb182
DiegoGranadosLy <d.granadosly@gmail.com>
2018-08-06 13:25:58 -0600

    Problemas 1,3 y 4 de octave resueltos. Falta pdf con problema 1 y otros de 3 y 4

--- tarea1.m
+++ tarea1.m
@@ -7,9 +7,9 @@
 
 %%%%%%%%%%%%%%%%%%%%%%%%%Problema 1%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-#disp("Problema 1: \n")
-#x = input("Ingrese el valor de x: ");
-#c = input("Ingrese el numero de cifras significativas: ");
+disp("Problema 1: \n")
+x = input("Ingrese el valor de x: ");
+c = input("Ingrese el numero de cifras significativas: ");
 
 %Parametros de salida
 % v = valor estimado
@@ -21,66 +21,63 @@
 %Parametros de entrada
 % x = Valor real de entrada
 % c = Numero de cifras significativas
-#function [v,ev,ea,n] = anpi_erf(x,c)
-#  es=(0.5*(10^(2-c)));  % Umbral de Scarborough
-#  erf_real   = erf(x);   %Valor Real
-#  v_anterior =0;         %Valor anterior aproximado
-#  v  = (2/sqrt(pi))*(((-1)^0)/factorial(0))*((x^(2*0+1))/(2*0+1));
-#  ev = abs(erf_real-v)*100;       %Calculo del error verdadero
-#  ea = abs((v-v_anterior)/v)*100; %Calculo del error aproximado
-#  n = 1;                          % Numero de terminos (iteraciones)
+function [v,ev,ea,n] = anpi_erf(x,c)
+  es=(0.5*(10^(2-c)));  % Umbral de Scarborough
+  erf_real   = erf(x);   %Valor Real
+  v_anterior =0;         %Valor anterior aproximado
+  v  = (2/sqrt(pi))*(((-1)^0)/factorial(0))*((x^(2*0+1))/(2*0+1));
+  ev = abs(erf_real-v)*100;       %Calculo del error verdadero
+  ea = abs((v-v_anterior)/v)*100; %Calculo del error aproximado
+  n = 1;                          % Numero de terminos (iteraciones)
   
-#  while (es<ea)                     %Scarboough < error aproximado
-#    v  += (2/sqrt(pi))*(((-1)^n)/factorial(n))*((x^(2*n+1))/(2*n+1));
-#    ev = abs(erf_real-v)*100;       %Calculo del error verdadero
-#    ea = abs((v-v_anterior)/v)*100; %Calculo del error aproximado
-#    v_anterior = v;                 %Se actualiza el valor anterior
-#    n+=1;                           %Incrementa un termino para 
-#  endwhile                          %la siguiente iteracion.
-#endfunction
+  while (es<ea)                     %Scarboough < error aproximado
+    v  += (2/sqrt(pi))*(((-1)^n)/factorial(n))*((x^(2*n+1))/(2*n+1));
+    ev = abs(erf_real-v)*100;       %Calculo del error verdadero
+    ea = abs((v-v_anterior)/v)*100; %Calculo del error aproximado
+    v_anterior = v;                 %Se actualiza el valor anterior
+    n+=1;                           %Incrementa un termino para 
+  endwhile                          %la siguiente iteracion.
+endfunction
 
 
-#[v,ev,ea,n] =  anpi_erf(x,c)
-#printf("Fin del ejercicio 1 \n");
+[v,ev,ea,n] =  anpi_erf(x,c)        %Llamada a la funcion y retorno de parametros
 
 %%%%%%%%%%%%%%%%%%%%%%%%%Problema 2%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-#disp("\n Problema 2: \n")
-%POR HACER
+%Problema 2 en el pdf
 
 %%%%%%%%%%%%%%%%%%%%%%%%%Problema 3%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-#disp("\n Problema 3: \n")
+disp("\n Problema 3: \n")
 
-#disp("\n output_precision(30) incrementa la precision");
-#disp(" de salida de las variables usadas");
-#output_precision(30);
-#disp("\n 0.1 con precision de float mas cercano representable");
-#a=single(0.1)
-#disp("\n 0.1 con precision de double mas cercano representable");
-#b=double(0.1)
-#disp("\n Diferencia no da cero por las representaciones numericas");
-#disp(" de la presicion simple y double. Ambas tienen un delta diferente");
-#disp(" es la diferencia mostrada en la resta.");
-#double(a)-b
+disp("\n output_precision(30) incrementa la precision");
+disp(" de salida de las variables usadas");
+output_precision(30);
+disp("\n 0.1 con precision de float mas cercano representable");
+a=single(0.1)
+disp("\n 0.1 con precision de double mas cercano representable");
+b=double(0.1)
+disp("\n Diferencia no da cero por las representaciones numericas");
+disp(" de la precision simple y double. Ambas tienen un delta diferente");
+disp(" es la diferencia mostrada en la resta.");
+double(a)-b
 
-#printf("Fin del ejercicio 3 \n");
 %%%%%%%%%%%%%%%%%%%%%%%%%Problema 4%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 disp("\n Problema 4: \n")
 fod = input("Introducir 1 para float, 2 para double: ");
 
-if (fod==1)
+if (fod==1) %Precision simple (float)
   printf("Precision simple (float) \n");
   a=single(input("Valor de a: "));
   b=single(input("Valor de b: "));
   c=single(input("Valor de c: "));
-elseif (fod==2)
+elseif (fod==2) %Precision doble (double)
   printf("Precision doble (double) \n");
   a=double(input("Valor de a: "));
   b=double(input("Valor de b: "));
   c=double(input("Valor de c: "));
-else
+else   %En caso de digitar una entrada invalida que float por defecto
   printf("Precision simple por default \n");
   a=single(input("Valor de a: "));
   b=single(input("Valor de b: "));
